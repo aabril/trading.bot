2bfd7e7229b5ccf638107f85990b5ade5b5147e1
Albert Abril <albert@Alberts-MacBook-Pro.local>
2019-11-23 01:27:43 +0000

    \ • Adding the welcome message \ • Adding keylistener to display the current PnL \ \

--- src/start.js
+++ src/start.js
@@ -2,12 +2,37 @@ const EventEmitter = require('events');
 const main = require('./main')
 const msInterval = 1000
 const pnl = 50000
+const readline = require('readline');
 
 class Poller extends EventEmitter {
     constructor(cb = ()=>{} , timeout = 100) {
         super();
+        this.printWelcomeMessage()
         this.timeout = timeout
         this.pnl = pnl
+        this.keyListener()
+    }
+
+    printWelcomeMessage() {
+        console.log('')
+        console.log('Welcome to the Bitfinex Trader Bot')
+        console.log('> Press P to check your PNL at any time')
+        console.log('> Press Control+C to stop the bot')
+        console.log('')
+    }
+
+    keyListener() {
+        readline.emitKeypressEvents(process.stdin);
+        process.stdin.setRawMode(true);
+        process.stdin.on('keypress', (str, key) => {
+            if (key.ctrl && key.name === 'c') {
+                process.exit();
+            } else {
+                if(key.sequence === 'p') {
+                    console.log(this.pnl)
+                }
+            }
+        })
     }
 
     poll() {  
