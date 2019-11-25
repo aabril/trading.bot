2d3f74377434d9d662baee0671493bf59e1a942b
Albert Abril <albert@Alberts-MacBook-Pro.local>
2019-11-25 16:41:09 +0000

    mocking this.orders in orders

--- src/Orders.js
+++ src/Orders.js
@@ -7,14 +7,29 @@ class Orders {
                 "amount": 1.53,
                 "totalAmount": 10710,
                 "pair": "BTCUSD"
-            }
+            },
+            {
+                "order": "buy",
+                "orderPrice": 9000,
+                "amount": 1.93,
+                "totalAmount": 14700,
+                "pair": "BTCUSD"
+            },
+            {
+                "order": "sell",
+                "orderPrice": 2000,
+                "amount": 0.93,
+                "totalAmount": 8569,
+                "pair": "BTCUSD"
+            }            
         ]
     }
 
     getOrdersMsgs() {
         const msgs = []
         this.orders.forEach(element => {
-            let msg = `${element.order} ${element.amount} ${element.pair.substring(0,3)} at price of ${element.orderPrice} ${element.pair.substring(3,3)}. Total amount: ${element.amount} }`
+            let msg = `${element.order} ${element.amount} ${element.pair.substring(0,3)} at price of ${element.orderPrice} ${element.pair.substring(3,3)}. Total amount: ${element.amount}`
+            console.log(msg)
             msgs.push[msg]
         });
         return msgs
